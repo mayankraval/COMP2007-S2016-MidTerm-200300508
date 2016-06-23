1b13b1c07b666d37a99dd19a5885530b3a11eef8
mayankraval <mayank.raval8293@gmail.com>
2016-06-23 14:47:49 -0400

    Initial commit

---
+++ COMP2007-S2016-MidTerm-200300508/TodoDetails.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+// using statements that are required to connect to EF DB
+using COMP2007_S2016_MidTerm_200300508.Models;
+using System.Web.ModelBinding;
+using System.Linq.Dynamic;
+
+namespace COMP2007_S2016_MidTerm_200300508
+{
+    public partial class TodoDetails : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if ((!IsPostBack) && (Request.QueryString.Count > 0))
+            {
+                this.GetTodo();
+            }
+        }
+
+        protected void GetTodo()
+        {
+            // populate teh form with existing data from the database
+            int TodoID = Convert.ToInt32(Request.QueryString["TodoID"]);
+
+            // connect to the EF DB
+            using (TodoConnection db = new TodoConnection())
+            {
+                // populate a student object instance with the StudentID from the URL Parameter
+                Todo updatedTodo = (from todo in db.Todos
+                                          where todo.TodoID == TodoID
+                                          select todo).FirstOrDefault();
+
+                // map the student properties to the form controls
+                if (updatedTodo != null)
+                {
+                    TodoNameTextBox.Text = updatedTodo.TodoName;
+                    TodoNotesTextBox.Text = updatedTodo.TodoNotes;
+                    TodoCompletedCheckbox.Checked = updatedTodo.Completed.Value;
+                }
+            }
+        }
+
+        protected void CancelButton_Click(object sender, EventArgs e)
+        {
+            // Redirect back to Students page
+            Response.Redirect("~/Contoso/Students.aspx");
+        }
+
+        protected void SaveButton_Click(object sender, EventArgs e)
+        {
+            // Use EF to connect to the server
+            using (TodoConnection db = new TodoConnection())
+            {
+                // use the Student model to create a new student object and
+                // save a new record
+                Todo newTodo = new Todo();
+
+                int TodoID = 0;
+
+                if (Request.QueryString.Count > 0) // our URL has a StudentID in it
+                {
+                    // get the id from the URL
+                    TodoID = Convert.ToInt32(Request.QueryString["TodoID"]);
+
+                    // get the current student from EF DB
+                    newTodo = (from todo in db.Todos
+                                  where todo.TodoID == TodoID
+                                  select todo).FirstOrDefault();
+                }
+
+                // add form data to the new student record
+                newTodo.TodoName= TodoNameTextBox.Text;
+                newTodo.TodoNotes = TodoNotesTextBox.Text;
+                newTodo.Completed = TodoCompletedCheckbox.Checked;
+
+                // use LINQ to ADO.NET to add / insert new student into the database
+
+                if (TodoID == 0)
+                {
+                    db.Todos.Add(newTodo);
+                }
+
+
+                // save our changes - also updates and inserts
+                db.SaveChanges();
+
+                // Redirect back to the updated students page
+                Response.Redirect("~/TodoList.aspx");
+            }
+        }
+    }
+}
---
+++ COMP2007-S2016-MidTerm-200300508/TodoList.aspx.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+// using statements that are required to connect to EF DB
+using COMP2007_S2016_MidTerm_200300508.Models;
+using System.Web.ModelBinding;
+using System.Linq.Dynamic;
+
+namespace COMP2007_S2016_MidTerm_200300508
+{
+    public partial class TodoList : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // if loading the page for the first time, populate the Game grid
+            if (!IsPostBack)
+            {
+                Session["SortColumn"] = "TodoID";
+                Session["SortDirection"] = "ASC";
+                // Get the student data
+                this.GetTodo();
+            }
+
+        }
+        protected void GetTodo()
+        {
+            string sortString = Session["SortColumn"].ToString() + " " + Session["SortDirection"].ToString();
+
+            // connect to EF
+            using (TodoConnection db = new TodoConnection())
+            {
+                // query the Students Table using EF and LINQ
+                var g1 = (from allTodos in db.Todos
+                          select allTodos);
+
+                // bind the result to the GridView
+                TodoGridView.DataSource = g1.AsQueryable().OrderBy(sortString).ToList();
+                TodoGridView.DataBind();
+            }
+        }
+
+        protected void PageSizeDropDownList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // set new page size
+            TodoGridView.PageSize = Convert.ToInt32(PageSizeDropDownList.SelectedValue);
+
+            // refresh the grid
+            this.GetTodo();
+        }
+
+        protected void TodoGridView_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            // store which row was clicked
+            int selectedRow = e.RowIndex;
+
+            // get the selected StudentID using the Grid's DataKey collection
+            int TodoID = Convert.ToInt32(TodoGridView.DataKeys[selectedRow].Values["TodoID"]);
+
+            // use EF to find the selected student in the DB and remove it
+            using (TodoConnection db = new TodoConnection())
+            {
+                // create object of the Student class and store the query string inside of it
+                Todo deletedTodo = (from TodoRecords in db.Todos
+                                    where TodoRecords.TodoID == TodoID
+                                    select TodoRecords).FirstOrDefault();
+
+                // remove the selected student from the db
+                db.Todos.Remove(deletedTodo);
+
+                // save my changes back to the database
+                db.SaveChanges();
+
+                // refresh the grid
+                this.GetTodo();
+
+            }
+        }
+
+        protected void TodoGridView_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+
+            // Set the new page number
+            TodoGridView.PageIndex = e.NewPageIndex;
+
+            // refresh the grid
+            this.GetTodo();
+
+        }
+
+        protected void TodoGridView_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+
+            if (IsPostBack)
+            {
+                if (e.Row.RowType == DataControlRowType.Header) // check to see if the click is on the header row
+                {
+                    LinkButton linkbutton = new LinkButton();
+
+                    for (int index = 0; index < TodoGridView.Columns.Count; index++)
+                    {
+                        if (TodoGridView.Columns[index].SortExpression == Session["SortColumn"].ToString())
+                        {
+                            if (Session["SortDirection"].ToString() == "ASC")
+                            {
+                                linkbutton.Text = " <i class='fa fa-caret-up fa-lg'></i>";
+                            }
+                            else
+                            {
+                                linkbutton.Text = " <i class='fa fa-caret-down fa-lg'></i>";
+                            }
+
+                            e.Row.Cells[index].Controls.Add(linkbutton);
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        //hhifdhisadofhisadfh
+        protected void TodoGridView_Sorting(object sender, GridViewSortEventArgs e)
+        {
+
+            // get the column to sort by
+            Session["SortColumn"] = e.SortExpression;
+
+            // refresh the grid
+            this.GetTodo();
+
+            // toggle the direction
+            Session["SortDirection"] = Session["SortDirection"].ToString() == "ASC" ? "DESC" : "ASC";
+        }
+    }
+}
